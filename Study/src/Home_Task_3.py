f256c1e0c15658692c4d6c16a36f7ac98387bb83
Nykolab <nykolab@gmail.com>
2015-09-10 23:51:06 +0300

    Some changes: Home_Task_3

--- Study/src/Home_Task_3.py
+++ Study/src/Home_Task_3.py
@@ -1,16 +1,17 @@
 '''
-Write program which reads a file with simple html code and removes all tags 
-leaving only text (not a title, not a path to image or java script source). 
+Write program which reads a file with simple html code 
+and removes all tags 
+leaving only text 
+(not a title, not a path to image or java script source). 
 The result should be shown as found "visible" text lines printed out to the 
 screen in right order
-in_file = 
-with open(in_file) as f:
-    print f.readlines()
-
-
 '''
-
 import os
 
-BASE_DIR = os.path.dirname(__file__)
-print BASE_DIR
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+NAME = "text.file"
+FILE = BASE_DIR+"//"+NAME
+
+with open(FILE) as f:
+    print f.readlines()
+
