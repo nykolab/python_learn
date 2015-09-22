9406626c3dae5988f66ae62b891da2aeb5253e5a
Nykolab <nykolab@gmail.com>
2015-09-22 23:57:29 +0300

    Final Classes version 22.09.2015 23:58

--- Study/src/Classes.py
+++ Study/src/Classes.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
 class Snake(object):
+
     '''
     Write a module classes.py with declaration of the class describing 
     some animal by your choice. 
     Animal should have at least 2 attributes - name, hungry, color, etc.. 
     The class also should declare at least 3 methods:
-    
-    1. hello() – it should print some nice words describing object (animal) name 
-    
+    1. hello() – it should print some nice words describing object (animal) name  
     2. some "trick" action – sound or any other – for example for sound it 
     should print something randomly chosen sound - like a "bark" 
     or "gav-gav" for dog etc.
@@ -19,15 +19,60 @@ class Snake(object):
     After 3 "sound" action it should became hungry again and refuse to play tricks.
 
     '''
-    def __init__(self, name, color, venomous):
+
+    def __init__(self, name, color):
         self.name = name
         self.hungry = True
-        self.venomous = venomous
+        self.action = 0
+        self.color = color
     
     def hello(self):
-    
+        if self.action > 3:
+            self.hungry = True
+        if not self.hungry:
+            if self.name and self.color:
+                print "I'm {} snake, my name is {}".format(self.color, self.name)
+            elif self.name:
+                print "I'm snake, my name is {}".format(self.name)
+            elif self.color:
+                print "I'm {} snake".format(self.color)
+            else:
+                print "I'm {} snake"
+            self.action += 1
+        else:
+            print "You need to feed me before"
+        
     def bite(self):
+        if self.action > 3:
+            self.hungry = True
+        if not self.hungry:
+            print "I'm biting you!!!"
+            self.action += 1
+        else:
+            print "You need to feed me before"
     
     def feed(self):
+        if self.hungry:
+            print "Thanks"
+            self.hungry = False
+        else:
+            print "I'm not hungry"    
+
+# ===============================================
+
+if __name__ == '__main__':
+    a = Snake("Snakky","Black")
+    a.name = "Doggy"
+    a.hello()
+    a.bite()
+    a.bite()
+    a.bite()
+    a.bite()
+    a.feed()
+    a.hello()
+    a.bite()
+    a.bite()
+    a.bite()
+    a.bite()
     
     
